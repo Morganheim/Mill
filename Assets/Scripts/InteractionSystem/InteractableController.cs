85d13a26b710d9133874a9098d3da528e0b0f5db
Morganheim <timon.popovic@gmail.com>
2024-11-10 19:04:23 +0100

    Added interaction system
    - Added variation of Unity's EventTrigger class for handling pointer events on objects in the scene
    - Added custom inspector script for the controller class
    - Added data (scriptable object) script for setting up interactable data

---
+++ Assets/Scripts/InteractionSystem/InteractableController.cs
@@ -0,0 +1,121 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InteractableController : EventTrigger
+{
+    /**************************************** INSPECTOR VARIABLES ****************************************/
+    [SerializeField] private InteractableData _data;
+
+    /**************************************** PRIVATE METHODS ****************************************/
+    private void Execute(EventTriggerType id, BaseEventData eventData)
+    {
+        var triggerCount = triggers.Count;
+
+        for (int i = 0, imax = triggers.Count; i < imax; ++i)
+        {
+            var ent = triggers[i];
+            if (ent.eventID == id && ent.callback != null)
+                ent.callback.Invoke(eventData);
+        }
+    }
+
+    private bool IsTriggerable(PointerEventData eventData)
+    {
+        if (eventData.pointerPressRaycast.distance > _data.Distance && _data.Distance > 0)
+            return false;
+
+        return _data.AllowedMouseKeys.Contains(eventData.button);
+    }
+
+    /**************************************** EVENT TRIGGER CALLBACKS ****************************************/
+    /// <summary>
+    /// Called by the EventSystem when the pointer enters the object associated with this EventTrigger.
+    /// </summary>
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        Execute(EventTriggerType.PointerEnter, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem when the pointer exits the object associated with this EventTrigger.
+    /// </summary>
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        Execute(EventTriggerType.PointerExit, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem when a PointerDown event occurs.
+    /// </summary>
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.PointerDown, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem when a PointerUp event occurs.
+    /// </summary>
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.PointerUp, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem when a Click event occurs.
+    /// </summary>
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.PointerClick, eventData);
+    }
+
+    /// <summary>
+    /// Called before a drag is started.
+    /// </summary>
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.BeginDrag, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem every time the pointer is moved during dragging.
+    /// </summary>
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.Drag, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem once dragging ends.
+    /// </summary>
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        if (!IsTriggerable(eventData))
+            return;
+
+        Execute(EventTriggerType.EndDrag, eventData);
+    }
+
+    /// <summary>
+    /// Called by the EventSystem when a new Scroll event occurs.
+    /// </summary>
+    public override void OnScroll(PointerEventData eventData)
+    {
+        Execute(EventTriggerType.Scroll, eventData);
+    }
+}
---
+++ Assets/Scripts/InteractionSystem/InteractableData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[CreateAssetMenu(fileName = "InteractableData", menuName = "Mill/Data/Interactable Data")]
+public class InteractableData : ScriptableObject
+{
+    [field: SerializeField] public PointerEventData.InputButton[] AllowedMouseKeys { get; private set; }
+    [field: SerializeField, Tooltip("Ray distance from camera near clip plane")] public float Distance { get; private set; }
+}
