56ff4336d3893958942e618ca1d8ef88f192cc7a
Morganheim <timon.popovic@gmail.com>
2024-11-14 00:53:53 +0100

    Added input controller
    - Added input controller for handling and interpreting input action events

---
+++ Assets/Scripts/InputEventController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputEventController : MonoBehaviour
+{
+    [SerializeField] private GameEventEmitter _emitter;
+
+    private bool _isPaused = false;
+    private bool _isGameOver = false;
+
+    private void OnEnable()
+    {
+        _isGameOver = false;
+        _isPaused = false;
+    }
+
+    public void OnPauseInputReceived(InputAction.CallbackContext context)
+    {
+        if (!context.performed || _isGameOver)
+            return;
+
+        _isPaused = !_isPaused;
+
+        _emitter.Emit(!_isPaused ? "OnPauseRequested" : "OnUnpauseRequested");
+    }
+
+    public void OnDeselectPieceInputReceived(InputAction.CallbackContext context)
+    {
+        if (!context.performed || _isGameOver)
+            return;
+
+        _emitter.Emit("OnDeselectPiece");
+    }
+
+    public void OnGameComplete()
+    {
+        _isGameOver = true;
+    }
+}
